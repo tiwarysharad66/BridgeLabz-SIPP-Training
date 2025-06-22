7c3dd1ef97274995d34871173fd4796abd9787ce
tiwarysharad66 <112773889+tiwarysharad66@users.noreply.github.com>
2025-06-22 20:18:15 +0530

    Added:Write a Program to ReplaceWord

---
+++ ExtrasJavaStringsPractice/ReplaceWord.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class ReplaceWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+        System.out.print("Enter word to replace: ");
+        String oldWord = sc.next();
+        System.out.print("Enter new word: ");
+        String newWord = sc.next();
+
+        String result = sentence.replaceAll("\b" + oldWord + "\b", newWord);
+        System.out.println("Modified sentence: " + result);
+    }
+}
