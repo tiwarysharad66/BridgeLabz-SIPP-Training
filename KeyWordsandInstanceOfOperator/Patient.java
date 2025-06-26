3728981ebbedcd9607bc2630a0d0db351ab6b705
tiwarysharad66 <112773889+tiwarysharad66@users.noreply.github.com>
2025-06-26 23:40:49 +0530

    Added:Program for Hospital Management System

---
+++ KeyWordsandInstanceOfOperator/Patient.java
@@ -0,0 +1,31 @@
+public class Patient {
+    private static String hospitalName = "City Hospital";
+    private static int totalPatients = 0;
+
+    private final int patientID;
+    private String name;
+    private int age;
+    private String ailment;
+
+    public Patient(int patientID, String name, int age, String ailment) {
+        this.patientID = patientID;
+        this.name = this.name = name;
+        this.age = this.age = age;
+        this.ailment = this.ailment = ailment;
+        totalPatients++;
+    }
+
+    public static void getTotalPatients() {
+        System.out.println("Total Patients: " + totalPatients);
+    }
+
+    public void displayDetails() {
+        if (this instanceof Patient) {
+            System.out.println("Hospital: " + hospitalName);
+            System.out.println("Patient ID: " + patientID);
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("Ailment: " + ailment);
+        }
+    }
+}
