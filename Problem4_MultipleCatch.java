a1aa658a5b6b2b17bd7f44587dc63a814e5e315e
tiwarysharad66 <112773889+tiwarysharad66@users.noreply.github.com>
2025-08-19 11:07:35 +0530

    Added: Multiple Catch

---
+++ Problem4_MultipleCatch.java
@@ -0,0 +1,14 @@
+public class Problem4_MultipleCatch {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30};
+
+        try {
+            int index = 5;
+            System.out.println("Value at index " + index + ": " + arr[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+    }
+}
