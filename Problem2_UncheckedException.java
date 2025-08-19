5dd1e582bdc319a7b0661fd05d4a7efaea04abef
tiwarysharad66 <112773889+tiwarysharad66@users.noreply.github.com>
2025-08-19 11:06:56 +0530

    Added: UncheckedExceptions

---
+++ Problem2_UncheckedException.java
@@ -0,0 +1,20 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Problem2_UncheckedException {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter numerator: ");
+            int num1 = sc.nextInt();
+            System.out.print("Enter denominator: ");
+            int num2 = sc.nextInt();
+            int result = num1 / num2;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero!");
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input! Please enter numeric values.");
+        }
+    }
+}
